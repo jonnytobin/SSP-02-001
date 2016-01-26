6a0d59a1cd645b808ab3df18da4117cca5fa6e2f
Jonathan Tobin <jonny.tobin24@gmail.com>
2016-01-26 16:00:48 +0000

    Class work (Not completeyet)

--- myProg.js
+++ myProg.js
@@ -1,38 +1,39 @@
 // Getting the file system (FS) from node.
 var fs = require("fs");
-var fileContents = new Array();
 var count;
 count = 0;
 
-/*fs.readdir("./", function(err, files){
+fs.readdir("./", function(err, files)
+{
 	console.log("Got the contents of the directory.");
 	// Loop through the array, there are a few ways of going this
-	for(var i in files) {
+	
+	for(var i in files) 
+	{
 		console.log("File: " + files[i]);
-	}
-}	
-);*/
-
-
-
-fs.readFile("myFile_01.txt", {encoding: 'utf8', flag: 'r'}, function(error, data) {
-	// If we have an error object then throw it. JS has an inbuild Error object
-	// so look it up to learn more about it.
-	if (error){
-		// The throw statement in JS throws an exception which other code can catch
-		// and try yo handle. It is a form or error handling.
-		throw error;
-	}
-	 
-    fs.writeFile("app-messages.txt", data, function(error){
-        if (error) throw error;
-
-        console.log("Message written to file");
-    });
+		
+		if(files[i] != "myProg.js") 
+		{
+			console.log(files[i]);
+			fs.readFile(files[i], function(error, data) 
+			{
+				// If we have an error object then throw it. JS has an inbuild Error object
+				// so look it up to learn more about it.
+				console.log("Contents of file: " + files[i] + " " + data);	
+				if (error)
+				{
+					// The throw statement in JS throws an exception which other code can catch
+					// and try yo handle. It is a form or error handling.
+					throw error;
+				}
     
-	console.log("Contents of file: " + data);
-});
+			
+			});
+		}
+		count++;
+		if(count == 3) {
 
+		}
+	}
 
-
-
+});  // Note that this bracket closes readdir method call
